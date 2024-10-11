5fc97b67b1a9dd32f58e30c5207965eca5a732cb
Sixze <16257871+Sixze@users.noreply.github.com>
2024-10-12 01:18:20 +0300

    Make the character automatically turn in the view direction when standing still by pressing any movement keys

--- Source/ALS/Public/Settings/AlsCharacterSettings.h
+++ Source/ALS/Public/Settings/AlsCharacterSettings.h
@@ -34,6 +34,7 @@ public:
 	uint8 bInheritMovementBaseRotationInVelocityDirectionRotationMode : 1 {false};
 
 	// If checked, the character will rotate towards the direction they want to move, but is not always able to due to obstacles.
+	// This setting is only used if the bIgnoreBaseRotation setting from the character movement component is set to true.
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Settings")
 	uint8 bRotateTowardsDesiredVelocityInVelocityDirectionRotationMode : 1 {true};
 
